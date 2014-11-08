d1d2e3a3e37053df7f754908e8c69e3e62b3e831
ronbarr <ron.barr@icloud.com>
2014-11-07 21:55:36 -0800

    Initial Commit

---
+++ dropBoxer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dropBoxer
+//
+//  Created by Ron Barr on 11/7/14.
+//  Copyright (c) 2014 Ron Barr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dropBoxer/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  dropBoxer
+//
+//  Created by Ron Barr on 11/7/14.
+//  Copyright (c) 2014 Ron Barr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
